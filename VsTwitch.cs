e75a9e85eb82e2d9e4c813607083f942f3d339a7
Justin Derby <justderb@gmail.com>
2020-12-11 15:25:54 -0800

    Bump version

--- VsTwitch.cs
+++ VsTwitch.cs
@@ -20,7 +20,7 @@ namespace VsTwitch
         private static readonly char[] SPACE = new char[] { ' ' };
         public const string GUID = "com.justinderby.vstwitch";
         public const string ModName = "Vs Twitch";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
 
         // This is only used for ConCommands, since they need to be static...
         public static VsTwitch Instance;
