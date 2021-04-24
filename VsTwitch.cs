30ba6012074e39a8a364c89501ac4d4402dd9aab
Justin Derby <justderb@gmail.com>
2021-04-23 20:12:36 -0700

    Uploaded wrong files for 1.0.7

--- VsTwitch.cs
+++ VsTwitch.cs
@@ -21,7 +21,7 @@ namespace VsTwitch
         private static readonly char[] SPACE = new char[] { ' ' };
         public const string GUID = "com.justinderby.vstwitch";
         public const string ModName = "VsTwitch";
-        public const string Version = "1.0.7";
+        public const string Version = "1.0.8";
 
         // This is only used for ConCommands, since they need to be static...
         public static VsTwitch Instance;
